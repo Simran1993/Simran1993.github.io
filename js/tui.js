288263900438a93334ff6864dd94b92cdc576ddc
Simran1993 <harsimranjits5102@gmail.com>
2025-08-09 03:16:15 -0400

    4th commit

--- js/tui.js
+++ js/tui.js
@@ -362,7 +362,6 @@ async function displayContent() {
     outerContainerElement.appendChild(innerContainerElement);
     MAIN_CONTENT_SECTION.appendChild(outerContainerElement);
 
-    await displayRandomBibleVerse(innerContainerElement);
   }
 }
 
